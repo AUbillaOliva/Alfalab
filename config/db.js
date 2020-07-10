99b31625a464603103af2a9e4e35cebd8cd97d8e
AUbillaOliva <afubillaoliva@gmail.com>
2020-07-10 05:56:00 -0400

    :wrench: using env vars

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoUri');
-//const autoIncrement = require('mongoose-auto-increment');
+const db = process.env.MONGO_URI;
 
 
 const connectDB = async (req, res) => {
@@ -10,7 +9,6 @@ const connectDB = async (req, res) => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    //autoIncrement.initialize(connection);
     console.log('db connected');
   } catch(err) {
     console.error(err);
