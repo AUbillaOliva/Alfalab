5125d6c5aa44bd2b543556de7a86fbc9bf18fa3e
AUbillaOliva <afubillaoliva@gmail.com>
2020-07-12 11:22:16 -0400

    :twisted_rightwards_arrows: merging with api branch

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-const config = require('config');
 const db = process.env.MONGO_URI;
 
-const connectDB = async (req, res) => {
+const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
     });
     console.log('db connected');
   } catch(err) {
