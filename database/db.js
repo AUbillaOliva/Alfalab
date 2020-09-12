c4108d00c66f0d3529eb2171204bb39222f314be
Álvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-09-11 22:17:57 -0300

    :twisted_rightwards_arrows: mergin files with api branch

--- database/db.js
+++ database/db.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 const db = process.env.MONGO_URI;
 
-const connectDB = async () => {
+const connectDB = async (req, res) => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true
+    }).catch((err) => {
+      process.stdout.write('\033c');
+      console.log(err);
+    }).then(() => {
+      console.log('Database connected');
     });
-    console.log('db connected');
   } catch(err) {
+    process.stdout.write('\033c');
     console.error(err);
   }
 }
