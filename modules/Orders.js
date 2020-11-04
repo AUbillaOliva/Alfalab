75033fccd25848a2753fb98b62cf7e881f321635
AUbillaOliva <afubillaoliva@gmail.com>
2020-11-03 22:21:14 -0300

    :twisted_rightwards_arrows: merging modules files from api branch

--- modules/Orders.js
+++ modules/Orders.js
@@ -3,43 +3,43 @@ const ClientSchema = require('./Clients');
 const OrderSchema = require('./Order');
 
 const OrdersSchema = new mongoose.Schema({
-  order: {
-      type: [OrderSchema],
-      required: true
-  },
-  client: {
-      type: ClientSchema,        
-      required: true
-  },     
-  zone: {
-      type: String,
-      required: false,
-      lowercase: true
-  },
-  status: {
-      type: Boolean,
-      required: true
-  },
-  created_at: {
-      type: String,
-      required: true
-  },
-  orders_number : {
-      type: Number,
-      unique: true,
-      ref: 'orders_number',
-      lowercase: true
-  },
-  delivered_by: {
-      type: String,
-      lowercase: true
-  },
-  delivered_date: {
-      type: String
-  },
-  total: {
-      type: String
-  }
+    order: {
+        type: [OrderSchema],
+        required: true
+    },
+    client: {
+        type: ClientSchema,        
+        required: true
+    },     
+    zone: {
+        type: String,
+        required: false,
+        lowercase: true
+    },
+    status: {
+        type: Boolean,
+        required: true
+    },
+    created_at: {
+        type: String,
+        required: true
+    },
+    orders_number : {
+        type: Number,
+        unique: true,
+        ref: 'orders_number',
+        lowercase: true
+    },
+    delivered_by: {
+        type: String,
+        lowercase: true
+    },
+    delivered_date: {
+        type: String
+    },
+    total: {
+        type: String
+    }
 });
 
 module.exports = OrdersSchema;
