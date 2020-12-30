3679bd3012c3d57fa419f74adcfcd7692427e1b3
AUbillaOliva <afubillaoliva@gmail.com>
2020-12-29 23:35:44 -0300

    :zap: General project update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('newrelic');
-const express = require('express');
+require("newrelic");
+const express = require("express");
 const app = express();
-const autoincrement = require('mongoose-auto-increment');
-const mongoose = require('mongoose');
+const autoincrement = require("mongoose-auto-increment");
+const mongoose = require("mongoose");
 
-const connectDB  = require('./database/db.js');
+const connectDB = require("./database/db.js");
 
 connectDB();
 autoincrement.initialize(mongoose.connection);
@@ -13,11 +13,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use('/api/delivered', require('./routes/api/delivered'));
-app.use('/api/orders', require('./routes/api/orders'));
-app.use('/support/report', require('./routes/support/report'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/users', require('./routes/api/users'));
+app.use("/api/delivered", require("./routes/api/delivered"));
+app.use("/api/orders", require("./routes/api/orders"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/messages", require("./routes/api/messages"));
+
+app.use("/support/report", require("./routes/support/report"));
 
 app.listen(PORT, () => {
   console.log(`~ Server running on port ${PORT}`);
